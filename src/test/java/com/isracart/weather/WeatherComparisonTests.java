afaf5f2b9a30072287b23c6409e95f54218fd969
ron elkayam <ronelk6@gmail.com>
2024-12-31 15:38:30 +0200

    support

--- src/test/java/com/isracart/weather/WeatherComparisonTests.java
+++ src/test/java/com/isracart/weather/WeatherComparisonTests.java
@@ -1,18 +1,21 @@
 package com.isracart.weather;
 import com.isracart.api.WeatherApi;
+import com.isracart.common.SeleniumBase;
 import com.isracart.pages.WeatherPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class WeatherComparisonTests {
+public class WeatherComparisonTests extends SeleniumBase {
 
     WeatherApi weatherApi = new WeatherApi();
+
+    WeatherPage weatherPage = new WeatherPage(getDriver());
 
     @Test
     public void testTemperatureComparison() throws InterruptedException {
 
         double apiTemperature = weatherApi.getTemperatureFromWeatherAPI();
-        double seleniumTemperature = WeatherPage.getTemperatureFromWeatherUI();
+        double seleniumTemperature = weatherPage.getTemperatureFromWeatherUI();
         double difference = Math.abs(seleniumTemperature - apiTemperature);
         double maxAllowedDifference = 10.0;
 
